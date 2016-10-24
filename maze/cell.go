460636fa4eeaba2149c080c3b43ac5f576aa2482
weiwufeng <weiwufeng@sohu.com>
2016-10-24 22:08:06 +0800

    ...

--- maze/cell.go
+++ maze/cell.go
@@ -1,7 +1,7 @@
 package maze
 
 type Cell struct {
-	Data byte
+	_data byte
 }
 
 func NewCell(d byte) *Cell {
@@ -9,49 +9,41 @@ func NewCell(d byte) *Cell {
 }
 
 func (c *Cell)HasTop()  bool {
-	side:=c.Data&0x08
-
-	return side==8
+	return c._data&0x08==8
 }
 
 
 func (c *Cell)HasRight()  bool {
-	side:=c.Data&0x4
-
-	return side==4
+	return c._data&0x4 ==4
 }
 
 func (c *Cell)HasBottom()  bool {
-	side:=c.Data&0x2
-
-	return side==2
+	return c._data&0x2==2
 }
 
 func (c *Cell)HasLeft()  bool {
-	side:=c.Data&0x1
-
-	return side==1
+	return c._data&0x1 == 1
 }
 
 func (c *Cell)EraseLeft() {
-	c.Data=c.Data&0xfe
+	c._data =c._data&0xfe
 }
 
 func (c *Cell)EraseRight() {
-	c.Data=c.Data&0xfb
+	c._data =c._data&0xfb
 }
 
 
 func (c *Cell)EraseTop() {
-	c.Data=c.Data&0xf7
+	c._data =c._data&0xf7
 }
 
 func (c *Cell)EraseBottom() {
-	c.Data=c.Data&0xfd
+	c._data =c._data&0xfd
 }
 
 func (c *Cell)IsClosed() bool {
-	return c.Data==0xff || c.Data==0x0f
+	return c._data==0xff || c._data==0x0f
 }
 
 
