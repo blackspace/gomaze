2ac3a9bbead36e67a42a9e115cb8c369e96bf8e3
weiwufeng <weiwufeng@sohu.com>
2016-10-27 12:41:41 +0800

    ...

--- maze/point_set.go
+++ maze/point_set.go
@@ -1,5 +1,7 @@
 package maze
 
+import "strconv"
+
 type PointSet struct {
 	_data []int64
 }
@@ -45,4 +47,36 @@ func (p *PointSet)Join(jp *PointSet) {
 		x,y:=jp.Index(i)
 		p.Add(x,y)
 	}
+}
+
+func (p *PointSet)Equal(tp *PointSet) bool {
+	if len(p._data)!=len(tp._data) {
+		return false
+	}
+
+	for i:=0;i<len(p._data);i++ {
+		x,y:=p.Index(i)
+
+		if !tp.HasPoint(x,y) {
+			return false
+		}
+	}
+
+	for i:=0;i<len(tp._data);i++ {
+		x,y:=tp.Index(i)
+
+		if !p.HasPoint(x,y) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func (p *PointSet)String() (result string) {
+	for i:=0;i<p.Count();i++ {
+		x,y:=p.Index(i)
+		result=result+"("+strconv.Itoa(x)+","+strconv.Itoa(y)+")"
+	}
+	return  result
 }
